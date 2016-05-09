5a624f7a6d85e4174ed0109b41a53763397207a5
dreadnoughtsix <caba.james@yahoo.ca>
2016-05-09 15:22:24 -0400

    Create Ball variables and methods
    Initialize public integers "x" and "y" as x-position and y-position of the
    ball respectively. Initialize the private Dimension "ballDim" with the
    size of the ball.
    
    Create Ball initializer and point "this.x" and "this.y" to the middle of
    the frame.
    
    In drawBall(), set the color and draw the rectangle relative to "this.x"
    and "this.y" and the ball's dimensions.

--- Ball.java
+++ Ball.java
@@ -5,6 +5,20 @@
  */
 
 import java.awt.Graphics;
+import java.awt.Dimension;
+import java.awt.Color;
 
 public class Ball {
+    public int x, y;
+    private Dimension ballDim = new Dimension(15, 15);
+
+    Ball() {
+        this.x = (GameFrame.WIDTH / 2) - ballDim.width;
+        this.y = (GameFrame.HEIGHT / 2) - ballDim.height;
+    }
+
+    public void drawBall(Graphics g) {
+        g.setColor(Color.WHITE);
+        g.fillRect(this.x, this.y, ballDim.width, ballDim.height);
+    }
 }
