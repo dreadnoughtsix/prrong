eb447972d7cce99c2e266bbf126b93c60bbfb831
dreadnoughtsix <caba.james@yahoo.ca>
2016-05-10 16:42:24 -0400

    Create Paddle variables and methods
    Initialize Paddle's final width and height to be used to draw it.
    Initialize "x" and "y" as the Paddle's initial drawing position on the
    GameFrame's grid.
    
    Create Ball constructor with "player" parameter. Create if statements to
    check if "player" is 1 or 2. This is needed for the GameController and
    to allow different "x" positions for the paddle is to be drawn
    initially.
    
    In drawPaddle(), set paddle's color to be white to be seen on the black
    background, and fill the rectangle according to the current paddle's "x"
    and "y" position.

--- Paddle.java
+++ Paddle.java
@@ -4,10 +4,28 @@
  * @author dreadnoughtsix
  */
 
-import java.awt.*;
-import javax.swing.*;
-import java.awt.event.*;
+import java.awt.Graphics;
+import java.awt.Color;
 
 public class Paddle {
+    
+    public final int PADDLE_WIDTH = 10, PADDLE_HEIGHT = GameFrame.HEIGHT/10;
+    private int x, y, player;
 
+    Paddle(int player) {
+        this.player = player;
+
+        if (this.player == 1) {
+            this.x = 0;
+        } else if (this.player == 2) {
+            this.x = GameFrame.WIDTH - PADDLE_WIDTH;
+        }
+
+        this.y = 0;
+    }
+
+    public void drawPaddle(Graphics g) {
+        g.setColor(Color.WHITE);
+        g.fillRect(this.x, this.y, PADDLE_WIDTH, PADDLE_HEIGHT);
+    }
 }
