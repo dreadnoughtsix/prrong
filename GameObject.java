b9fccdcba583fd6d235deb7ff7a5e1fbcff14312
dreadnoughtsix <caba.james@yahoo.ca>
2016-05-11 17:26:02 -0400

    Add necessary GameObject methods
    Initialize GameObject x and y variables and values in constructor with
    x and y as parameters. Create setters and getters for both variables.
    These methods are to be used for change of grid location of the current
    GameObject, which is designed to be inherited.

--- GameObject.java
+++ GameObject.java
@@ -6,6 +6,26 @@
 
 public class GameObject {
 
-    GameObject() {
+    public int x, y;
+
+    GameObject(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getY() {
+        return this.y;
     }
 }
