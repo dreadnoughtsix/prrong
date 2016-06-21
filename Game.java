1af1b2f678da627d123b99e2dfe15d1a7409d6e8
dreadnoughtsix <caba.james@yahoo.ca>
2016-06-20 20:32:20 -0400

    Implement Swing Timer
    Import Timer from swing to use for a swing Timer. Create a Timer "timer"
    variable. Create an ActionListener "taskPerformer" that is used as a
    parameter for creating the Timer. Initiate the delay value to be 1000
    (milliseconds). Call and create the new Timer to have the delay and
    taskPerformer. The Timer will be used to update and change the current
    frame when a paddle moves or when the ball moves.

--- Game.java
+++ Game.java
@@ -5,6 +5,9 @@
  */
 
 import javax.swing.JPanel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.Timer;
 import java.awt.Color;
 
 public class Game extends JPanel {
@@ -15,6 +18,8 @@ public class Game extends JPanel {
 
     private Paddle p1, p2;
     private Ball ball;
+    public Timer timer;
+    private int delay;
 
     Game() {
         this.setPreferredSize(GameFrame.gameDim);
@@ -23,6 +28,14 @@ public class Game extends JPanel {
         p1 = new Paddle(1);
         p2 = new Paddle(2);
         ball = new Ball();
+
+        ActionListener taskPerformer = new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                System.out.println("timer works");
+            }
+        };
+
+        timer = new Timer(delay, taskPerformer);
     }
 
 }
