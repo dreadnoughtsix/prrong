9858dc04eddf685ba3d0a14ff3097abe1498f19d
dreadnoughtsix <caba.james@yahoo.ca>
2016-06-25 22:55:07 -0400

    Remove GameFrame from Game
    GameFrame extends JFrame and Game extends JPanel. Having GameFrame
    inside the JPanel as a component is backwards. Remove GameFrame
    component from JPanel.

--- Game.java
+++ Game.java
@@ -5,21 +5,20 @@
  */
 
 import javax.swing.JPanel;
+import javax.swing.Timer;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import javax.swing.Timer;
 import java.awt.Color;
+import java.awt.Graphics;
 
 public class Game extends JPanel {
 
     private static final long serialVersionUID = 1L;
 
-    public GameFrame gameframe = new GameFrame();
-
     private Paddle p1, p2;
     private Ball ball;
     public Timer timer;
-    private int delay;
+    private int delay = 1000;
 
     Game() {
         this.setPreferredSize(GameFrame.gameDim);
@@ -36,6 +35,10 @@ public class Game extends JPanel {
         };
 
         timer = new Timer(delay, taskPerformer);
+        timer.start();
+    }
+
+    public void paintComponent(Graphics g) {
     }
 
 }
