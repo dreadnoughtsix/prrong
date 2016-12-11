ad402d6a8eec724b3cfb580c6664bfcacc703c39
dreadnoughtsix <caba.james@yahoo.ca>
2016-12-10 22:39:59 -0500

    Implement timer
    Import useful Timer packages from java.awt to use for a Timer in the
    Ball class.

--- Ball.java
+++ Ball.java
@@ -7,19 +7,25 @@
 import java.awt.Graphics;
 import java.awt.Dimension;
 import java.awt.Color;
+import javax.swing.Timer;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Ball extends GameObject {
 
     public int x, y;
     private Dimension ballDim = new Dimension(15, 15);
+    public Timer timer;
 
     Ball() {
         this.x = (GameFrame.WIDTH / 2) - ballDim.width;
-        this.y = (GameFrame.HEIGHT / 2) - ballDim.height;
+        this.y = (GameFrame.HEIGHT /2) - ballDim.height;
     }
 
     public void drawBall(Graphics g) {
         g.setColor(Color.WHITE);
         g.fillRect(this.x, this.y, ballDim.width, ballDim.height);
     }
+
+
 }
